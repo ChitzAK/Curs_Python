d65754f2cc6ec1123eb195fc136774e85fbea912
ChitzAK <82970883+ChitzAK@users.noreply.github.com>
2021-06-16 13:11:41 +0300

    Update USER.py

--- USER.py
+++ USER.py
@@ -11,30 +11,48 @@ def list_task():
     action = ''
 
     while action != 'Q':
-        action = str(input("Enter what you wish to do: "
-                           "'L' to List task "
-                           "'S' to Sort task "
-                           "'Q' to Quit")).upper()
+        action = str(input("Enter what you wish to do:\n'L' to List task\n'S' to Sort task\n'D' to Delete task or set "
+                           "as Done\n'Q' to Quit")).upper()
 
         if action == 'L':
+            # show only the list that has the user's name?
             print(to_does)
+
         elif action == 'S':
-            a = input(
-                "Enter how you want the list sorted: 'D' by due date; 'T' by task, 'N' by name or 'C' by category").upper()
-            if a == 'D':
-                to_does = to_does.sort_values(by='Due_Date')
-            elif a == 'T':
-                to_does = to_does.sort_values(by='Task')
-            elif a == 'N':
-                to_does = to_does.sort_values(by='Name')
-            elif a == 'C':
-                to_does = to_does.sort_values(by='Category')
+            b = input(
+                "Enter how you want the list sorted:\n'A' for Ascending\n'D' for Descending").upper()
+            if b == 'A':
+                a = input(
+                    "Enter how you want the list sorted:\n'D' by Due date\n'T' by Task\n'N' by Name\n'C' by Category").upper()
+                if a == 'D':
+                    to_does = to_does.sort_values(by='Due_Date')
+                elif a == 'T':
+                    to_does = to_does.sort_values(by='Task')
+                elif a == 'N':
+                    to_does = to_does.sort_values(by='Name')
+                elif a == 'C':
+                    to_does = to_does.sort_values(by='Category')
+            elif b == 'D':
+                a = input(
+                    "Enter how you want the list sorted:\n'D' by due date\n'T' by task\n'N' by name\n'C' by category").upper()
+                if a == 'D':
+                    to_does = to_does.sort_values(by='Due_Date', reverse=True)
+                elif a == 'T':
+                    to_does = to_does.sort_values(by='Task', reverse=True)
+                elif a == 'N':
+                    to_does = to_does.sort_values(by='Name', reverse=True)
+                elif a == 'C':
+                    to_does = to_does.sort_values(by='Category', reverse=True)
+        elif action == 'D':
+            del_nr = input('Enter the index number for the task you want to remove if it was done: ')
+            del_nr = int(del_nr)
+            to_does.drop([del_nr], inplace=True)
+
         elif action == 'Q':
             break
 
-    # to_does.to_csv('to_does.csv', index=False)
-    to_does = pd.read_csv('to_does.csv')
-    print(to_does)
+        to_does.to_csv('to_does.csv', index=False)
+        to_does = pd.read_csv('to_does.csv')
+        print(to_does)
 
     return None
-
